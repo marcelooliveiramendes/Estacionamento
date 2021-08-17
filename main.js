4e282479fd4961c76a7832441a1839216daf0f9c
Marcelo Oliveira <marcelooliveiraah09@gmail.com>
2021-08-17 18:13:56 -0300

    com JS

---
+++ main.js
@@ -0,0 +1,15 @@
+class Carro{
+    constructor (){
+        this.id = 0;
+        this.modelo = '';
+        this.placa = '';   
+    }
+    
+    saveCar(){
+        alert('Vamos salvar esse carro!');
+    }
+
+     deleteCar(){
+         alert('Vamos deletar esse carro!');
+    }
+}
