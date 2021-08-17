a1745ab2f258388a4a047272b7ce22a1927e888f
Marcelo Oliveira <marcelooliveiraah09@gmail.com>
2021-08-17 18:51:14 -0300

    com js part 1

--- main.js
+++ main.js
@@ -1,15 +1,50 @@
+
+
 class Carro{
     constructor (){
-        this.id = 0;
-        this.modelo = '';
-        this.placa = '';   
+        this.id = 1;
+        this.arrayCarros = [];
     }
     
-    saveCar(){
-        alert('Vamos salvar esse carro!');
-    }
+    salvar(){
+        let carro = this.lerDados();
 
-     deleteCar(){
+        if(this.validaCampos(carro)){
+            alert('Salvar');
+        }
+
+        console.log(carro);
+    }
+    cancelar(){
          alert('Vamos deletar esse carro!');
     }
+
+    lerDados(){
+        let carro = {};
+
+        carro.id = this.id;
+        carro.modelo = document.getElementById('modelo').value;
+        carro.placa = document.getElementById('placa').value;
+
+        return carro;
+    }
+    validaCampos(){
+        let msg = '';
+
+        if(carro.modelo == ''){
+            msg += '- Informe o modelo do carro \n';   
+        } if (carro.placa == ''){
+            msg += '- Informe a placa do carro \n';
+        } 
+
+        if(msg != ''){
+            alert(msg);
+            return false;
+        }
+
+        return true
+    }
+
 }
+
+var carro = new Carro;
