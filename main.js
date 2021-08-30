34755a9a66efde7a7ee91cc71ed8097c9a84e9ce
Marcelo Oliveira <marcelooliveiraah09@gmail.com>
2021-08-30 19:55:14 -0300

    JS Funcao Salvar

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ class Carro{
         this.id = 1;
         this.arrayCarros = [];
     }
+
+    salvar(){
+        let carro = this.lerDados();
+
+        if(this.validaCampos(carro)){
+            this.adicionar(carro);
+        }
+        
+        this.listarTabela();
+        this.limpaCampos();
+    }
+
 }
 
 var carro = new Carro();
